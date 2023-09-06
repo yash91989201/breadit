f9db1d5a25e8d62b246602928de890692b59cc74
yash91989201 <yashraj6370@gmail.com>
2023-09-06 16:42:01 +0530

    update redis config

--- src/components/EditorOutput.tsx
+++ src/components/EditorOutput.tsx
@@ -1,40 +1,40 @@
-'use client'
+"use client";
 
-import CustomCodeRenderer from '@/components/renderers/CustomCodeRenderer'
-import CustomImageRenderer from '@/components/renderers/CustomImageRenderer'
-import { FC } from 'react'
-import dynamic from 'next/dynamic'
+import CustomCodeRenderer from "@/components/renderers/CustomCodeRenderer";
+import CustomImageRenderer from "@/components/renderers/CustomImageRenderer";
+import { FC } from "react";
+import dynamic from "next/dynamic";
 
 const Output = dynamic(
-  async () => (await import('editorjs-react-renderer')).default,
+  async () => (await import("editorjs-react-renderer")).default,
   { ssr: false }
-)
+);
 
 interface EditorOutputProps {
-  content: any
+  content: any;
 }
 
 const renderers = {
   image: CustomImageRenderer,
   code: CustomCodeRenderer,
-}
+};
 
 const style = {
   paragraph: {
-    fontSize: '0.875rem',
-    lineHeight: '1.25rem',
+    fontSize: "0.875rem",
+    lineHeight: "1.25rem",
   },
-}
+};
 
 const EditorOutput: FC<EditorOutputProps> = ({ content }) => {
   return (
     <Output
       style={style}
-      className='text-sm'
+      className="text-sm"
       renderers={renderers}
       data={content}
     />
-  )
-}
+  );
+};
 
-export default EditorOutput
+export default EditorOutput;
